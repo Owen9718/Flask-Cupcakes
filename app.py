c2b7e3fa79f6d0ea6e99e6a12294b795c33de469
Owen <owen9718@outlook.com>
2022-05-16 17:01:17 -0400

    updating py

--- app.py
+++ app.py
@@ -1,1 +1,68 @@
 """Flask app for Cupcakes"""
+from crypt import methods
+from flask import Flask,jsonify,request,render_template
+
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = "oh-so-secret"
+
+connect_db(app)
+
+@app.route("/")
+def make_home():
+
+    return render_template("home.html")
+
+
+
+@app.route("/api/cupcakes")
+def all_cupcakes():
+    cupcakes = [cupcake.to_dict() for cupcake in Cupcake.query.all()]
+    
+    return jsonify(cupcakes=cupcakes)
+
+@app.route("/api/cupcakes/<int:id>")
+def get_cupcake(id):
+    cupcake = Cupcake.query.get_or_404(id)
+
+    return jsonify(cupcake=cupcake.to_dict())
+
+@app.route("/api/cupcakes", methods=["POST"])
+def add_cupcake():
+    data = request.json
+    cupcake = Cupcake(
+        flavor=data['flavor'],
+        size=data['size'],
+        rating=data['rating'],
+        image=data['image'] or None,
+    )
+    db.session.add(cupcake)
+    db.session.commit()
+    return (jsonify(cupcake=cupcake.to_dict()), 201)
+
+
+@app.route("/api/cupcakes/<int:id>", methods=['PATCH'])
+def update_cupcake(id):
+    data = request.json
+    cupcake = Cupcake.query.get_or_404(id)
+    cupcake.flavor=data['flavor']
+    cupcake.size=data['size']
+    cupcake.rating=data['rating']
+    cupcake.image=data['image'] or None
+
+    db.session.add(cupcake)
+    db.session.commit()
+    
+    return jsonify(cupcake=cupcake.to_dict())
+
+@app.route("/api/cupcakes/<int:id>", methods=['DELETE'])
+def delete_cupcake(id):
+    cupcake = Cupcake.query.get_or_404(id)
+    db.session.delete(cupcake)
+    db.session.commit()
+
+    return jsonify(message="Deleted")
--- models.py
+++ models.py
@@ -1,1 +1,37 @@
 """Models for Cupcake app."""
+
+from email.policy import default
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Cupcake(db.Model):
+    __tablename__ = "cupcakes"
+    id = db.Column(db.Integer,autoincrement=True,primary_key=True)
+    flavor = db.Column(db.Text,nullable=False)
+    size = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Float,nullable=False)
+    image = db.Column(db.Text,nullable=False,default="https://tinyurl.com/demo-cupcake")   
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "flavor": self.flavor,
+            "size": self.size,
+            "rating": self.rating,
+            "image": self.image
+        }
+
+
+
+
+
+
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
